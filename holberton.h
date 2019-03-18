1ccf33f3d9d2496626d683d1cba81ae8da62f3cb
chriswill88 <williechri79@gmail.com>
2019-03-18 17:37:40 -0400

    the header file has been added

--- holberton.h
+++ holberton.h
@@ -1,6 +1,17 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+/**
+* struct pairing - struct for pairing format to fuctions
+*
+* @a: the operator
+* @ptr: the fuction associated
+*/
+struct pairing
+{
+        char a;
+        void (*ptr)();
+};
 
 int _printf(const char *format, ...);
 
