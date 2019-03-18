acac82cea743ee24d0423347f0b301b73306b0ba
SeckMohameth <mohamethseck34@gmail.com>
2019-03-18 22:07:16 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -11,6 +11,15 @@ int _printf(const char *format, ...)
 	va_list arg;
 	unsigned int i;
 
+
+        struct paring[] = {
+                {'c', printf_c},
+                {'s', printf_s},
+                {'%', printf_f},
+                {NULL, NULL}
+	};
+
+
 	va_start(arg, format);
 
 	i = va_arg(arg, int);
